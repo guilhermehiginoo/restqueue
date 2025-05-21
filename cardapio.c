f87e56b87ebc27e32d4fd81ec1108f0128c884f7
Glauco Paiva <glaucopaiva013@gmail.com>
2025-05-21 04:51:42 +0000

    Feito bastante coisa, adicionando a função de processar a lista a fim de tranformá-la numa fila

--- cardapio.c
+++ cardapio.c
@@ -4,17 +4,17 @@
 void exibirCardapio(){
     printf("====== CARDÁPIO ======\n");
     printf("- Entradas - \n");
-    printf("1. Sopa de cebola\n 2.Salada Caesar \n 3. Bruschetta \n");
-    printf("4. Carpaccio de Carne\n 5. Camarão ao Alho \n");
-    printf("- Pratos principais -");
-    printf("6. Lasanha à bolonhesa\n 7. Filé Mignon com fritas \n 8. Frango grelhado com legumes\n");
-    print("9. Bacalhau à Gomes de Sá\n 10. Risoto de Cogumelos\n");
+    printf("1. Sopa de cebola\n2.Salada Caesar \n3. Bruschetta \n");
+    printf("4. Carpaccio de Carne\n5. Camarão ao Alho \n");
+    printf("- Pratos principais -\n");
+    printf("6. Lasanha à bolonhesa\n7. Filé Mignon com fritas \n8. Frango grelhado com legumes\n");
+    printf("9. Bacalhau à Gomes de Sá\n10. Risoto de Cogumelos\n");
     printf("- Sobremesas -\n");
-    printf("11. Tiramisu \n 12. Cheesecake de Frutas vermmelhas \n 13. Mousse de Chocolate \n 14. Pudim de Leite \n 15.Sorvete de baunilha com calda de morango \n")
+    printf("11. Tiramisu\n12. Cheesecake de Frutas vermmelhas\n13. Mousse de Chocolate\n14. Pudim de Leite\n15.Sorvete de baunilha com calda de morango\n");
 }
 
-char escolhaCardapio(int escolhido){
-    char cardapio[15] = {"Sopa de Cebola", "Salada Caesar", "Bruschetta", "Carpaccio de Carne",
+ char* escolhaCardapio(int escolhido){
+     char* cardapio[15] = {"Sopa de Cebola", "Salada Caesar", "Bruschetta", "Carpaccio de Carne",
          "Camarão ao Alho", "Lasanha à bolonhesa", "Filé Mignon com fritas", "Frango grelhado com legumes",
         "Bacalhau à Gomes de Sá", "Risoto de Cogumelos", "Tiramisu", "Cheesecake de Frutas vermelhas", "Mousse de Chocolate",
          "Pudim de Leite", "Sorvete de baunilha com calda de morango"};
---
+++ filaCozinha.c
@@ -0,0 +1,71 @@
+#include "filaCozinha.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Inicializa a lista, inicialmente com tamanho 0
+void inicializar_fila(Fila* fila)
+{
+  fila->tamanho = 0;
+}
+
+// Retorna a lista com tamanho 0
+int fila_vazia(Fila* fila)
+{
+  return fila->tamanho == 0;
+}
+
+// Retorna a lista com tamanho máximo
+int fila_cheia(Fila* fila)
+{
+  return fila->tamanho == TAMANHO_MAX;
+}
+
+// Insere um prato no fim da fila, seguindo o FIFO
+void inserir_fila(Fila* fila, char prato)
+{
+  if(fila_cheia(fila)){
+    printf("A fila está cheia!");
+  }else{
+    // Coloca o prato em dados, a partir da posição atual de tamanho e incrementa 1 do tamanho
+    // Assim, o prato é adicionado no fim da fila
+    fila->dados[fila->tamanho] = prato;
+    fila->tamanho++;
+  }
+}
+
+// Exibe os pratos na fila, se houver
+void exibir_fila(Fila* fila)
+{
+  // Verifica se a fila está vazia, se sim, imprime que nenhum pedido está sendo processado
+  if(fila_vazia(fila)){
+    printf("Nenhum pedido sendo processado\n");
+  }
+  // Se não, imprimie os pratos
+  else{
+    printf("Pedidos: \n");
+    for(int i = 0; i < fila->tamanho; i++){
+        printf("%c", fila->dados[i]);
+    }
+    printf("\n");
+  }
+}
+
+// Mecanismo importante para o projeto
+// Processa a fila de pedidos, copiando os pratos da lista de pedidos para a fila
+void processa_fila(Fila *fila, Pedido *lista) {
+  // Enquanto a lista não for nula, ou seja, enquanto não for vazia
+    while (lista != NULL) {
+        int pratoTamanho = strlen(lista->prato);
+        // strlen retorna o tamanho da string, ou seja, o número de caracteres
+        memcpy(&fila->dados[fila->tamanho], lista->prato, pratoTamanho);
+        // memcpy copia blocos de memória, de forma que memcpy(destino, origem, tamanho)
+        // Copia o prato da lista para a fila
+        fila->tamanho += pratoTamanho;
+        // Incrementa o tamanho da fila pelo tamanho do prato
+        // Adiciona um caractere de nova linha para separar os pratos
+        // Isso é importante para que o usuário saiba onde começa e termina cada prato
+        fila->dados[fila->tamanho++] = '\n';
+        // Avança para o próximo pedido na lista
+        lista = lista->proximo;
+    }
+}
