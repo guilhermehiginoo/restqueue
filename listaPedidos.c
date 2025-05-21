26fa456e3d7972ccdcff078eaa5fdc903c3b18de
Guilherme Higino <160296050+guilhermehiginoo@users.noreply.github.com>
2025-05-21 00:35:35 +0000

    Fixing some bugs and changing functions names

--- listaPedidos.c
+++ listaPedidos.c
@@ -1,130 +1,83 @@
-// Função de inserção no fim da lista.
-void insercao_no_fim_da_lsl(No **cabeca, int valor){
-  No *novo_no = malloc(sizeof(No));
-  novo_no->valor = valor;
-  novo_no->proximo = NULL;
+#include "listaPedidos.h"
 
-  if(*cabeca == NULL){
-    *cabeca = novo_no;
-    return;
-  }
-  
-  No *atual = *cabeca;
+// Função de inserção Pedido fim da lista.
+void anotarPedido(Pedido **cabeca, char *prato)
+{
+  Pedido *novoPedido = malloc(sizeof(Pedido));
+  int i = 0;
+    while (prato[i] != '\0' && i < (int)sizeof(novoPedido->prato) - 1) {
+        novoPedido->prato[i] = prato[i];
+        i++;
+    }
+    novoPedido->prato[i] = '\0';
+    novoPedido->proximo = NULL;
 
-  while(atual->proximo != NULL) {
-    atual = atual->proximo;
-  }
-  
-  atual->proximo = novo_no;
-}
-
-
-// Função de remoção no início da lista.
-void remocao_no_inicio_da_lsl(No **cabeca){
-  if(*cabeca == NULL){
-    printf("A lista está vazia!\n");
+  if (*cabeca == NULL)
+  {
+    *cabeca = novoPedido;
     return;
   }
 
-  No *temp = *cabeca;
-  *cabeca = (*cabeca)->proximo;
-  free(temp);
-}
+  Pedido *atual = *cabeca;
 
-
-// Função de remoção no fim da lista.
-void remocao_no_fim_da_lsl(No **cabeca){
-  if(*cabeca == NULL || (*cabeca)->proximo == NULL){
-    remocao_no_inicio_da_lsl(cabeca);
-    return;
-  }
-
-  No *anterior = NULL;
-  No *atual = *cabeca;
-
-  while(atual->proximo != NULL){
-    anterior = atual;
+  while (atual->proximo != NULL)
+  {
     atual = atual->proximo;
   }
 
-  anterior->proximo = NULL;
-
-  free(atual);
+  atual->proximo = novoPedido;
 }
-
 
 // Função de remoção em uma posição intermediária da lista.
-void remocao_no_meio_da_lsl(No **cabeca, int valor){
-  if(*cabeca == NULL){
+void cancelarPedido(Pedido **cabeca, char prato)
+{
+  if (*cabeca == NULL)
+  {
     printf("Lista vazia!\n");
     return;
   }
 
-  No *anterior = NULL;
-  No *atual = *cabeca;
+  Pedido *anterior = NULL;
+  Pedido *atual = *cabeca;
 
-  while(atual != NULL && atual->valor != valor){
+  while (atual != NULL && atual->prato != prato)
+  {
     anterior = atual;
     atual = atual->proximo;
   }
 
-  if(atual == NULL){
-    printf("Valor inexistente na lista!\n");
+  if (atual == NULL)
+  {
+    printf("Pedido não foi feito!\n");
     return;
   }
 
-  if(anterior == NULL){
+  if (anterior == NULL)
+  {
     *cabeca = atual->proximo;
-  }else{
+  }
+  else
+  {
     anterior->proximo = atual->proximo;
   }
 
   free(atual);
 }
 
-
-// Função de busca por valor na lista.
-void busca_por_valor_na_lsl(No *cabeca, int valor){
-  No *atual = cabeca;
-
-  while(atual != NULL && atual->valor != valor){
-    atual = atual->proximo;
-  }
-  
-  if(atual != NULL)
-    printf("Valor %d encontrado!\n", valor);
-  else
-   printf("Valor %d não encontrado!\n", valor);
-}
-
-
-// Função de varredura da lista.
-void percorre_a_lsl(No *cabeca){
-  No *atual = cabeca;
-  int total = 0;
-
-  while(atual != NULL){
-    total = total + atual->valor;
-    atual = atual->proximo;
-  }
-  printf("Total = %d.\n", total);
-}
-
-
-// Função de impressão de valores da lista.
-void imprime_a_lsl(No *cabeca) {
-  if(cabeca == NULL){
+void exibirPedidos(Pedido *cabeca)
+{
+  if (cabeca == NULL)
+  {
     printf("Lista vazia!\n");
     return;
   }
 
-  No *atual = cabeca;
-  
-  while(atual != NULL){
-    printf("%d ", atual->valor);
+  Pedido *atual = cabeca;
+
+  while (atual != NULL)
+  {
+    printf("%d ", atual->prato);
     atual = atual->proximo;
   }
   printf("\n");
 }
-
-  
--- listaPedidos.h
+++ listaPedidos.h
@@ -1,23 +1,20 @@
-#ifndef no
-#define no
+#ifndef LISTA_PEDIDOS_H
+#define LISTA_PEDIDOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 
-struct no {
-  int valor;
-  struct no *proximo;
-};
+typedef struct pedido {
+    char prato[100];
+    struct pedido *proximo;
+} Pedido;
 
-typedef struct no No;
+typedef struct pedido Pedido;
 
   
-void imprime_a_lsl(cabeca);
-void insercao_no_inicio_da_lsl(&cabeca, 10);
-void insercao_no_fim_da_lsl(&cabeca, 40);
-void insercao_no_meio_da_lsl(&cabeca, 25, 3);
-void remocao_no_inicio_da_lsl(&cabeca);
-void remocao_no_fim_da_lsl(&cabeca);
-void remocao_no_meio_da_lsl(&cabeca, 20);
-void busca_por_valor_na_lsl(cabeca, 70);
-void percorre_a_lsl(cabeca);
+void exibirPedidos(Pedido *cabeca);
+void anotarPedido(Pedido **cabeca, char *prato);
+void cancelarPedido(Pedido **cabeca, char *prato);
 
-  #endif
+#endif
