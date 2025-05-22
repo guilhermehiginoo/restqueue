ed9261dc77ff42ef4ba1dee7e08c7ff38cc10e53
Guilherme Higino <160296050+guilhermehiginoo@users.noreply.github.com>
2025-05-22 22:07:04 +0000

    Creating `cardapio.h` file and updating `cardapio.c`

---
+++ cardapio.h
@@ -0,0 +1,11 @@
+#ifndef CARDAPIO_H
+#define CARDAPIO_H
+
+#include <stdio.h>
+
+void exibirCardapio();
+const char *escolhaCardapio(int escolhido);
+
+// Funções para exibir e escolher pratos do cardápio, respectivamente
+
+#endif
